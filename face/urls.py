c51982e3ae52db9a47647279ca22ca6d2603e70d
Chinseok Lee <me@nomade.kr>
2018-05-17 02:51:11 +0900

    방문자가 볼 첫 페이지 화면 구현

--- face/urls.py
+++ face/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^person/(?P<pk>\d+).jpg$', views.person_photo, name='person_photo'),
 ]
 
--- face/views.py
+++ face/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.db.models import Count
+from django.http import Http404
+from django.shortcuts import redirect, render
+from .models import Person, Photo
 
-# Create your views here.
+
+def index(request):
+    person_qs = Person.objects.annotate(photo_count=Count('photo')).filter(photo_count__gt=0)
+
+    return render(request, 'face/index.html', {
+        'person_list': person_qs,
+    })
+
+
+def person_photo(request, pk):
+    photo = Photo.objects.filter(person__pk=pk).order_by('?').first()
+    if not photo:
+        raise Http404
+    return redirect(photo.file.url)
+
